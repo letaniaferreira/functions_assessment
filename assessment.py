7c7ec91a60cb5e7c82c0570c3c6dc9f62e44e9a9
Letania Ferreira <letaniaferreira@gmail.com>
2017-05-28 20:49:51 -0700

    functions assessment with first advanced exercise completed

---
+++ assessment.py
@@ -0,0 +1,235 @@
+"""
+Skills function assessment.
+
+Please read the the instructions first (separate file). Your solutions should
+go below this docstring.
+
+"""
+
+###############################################################################
+
+# PART ONE: Write your own function declarations.
+
+# NOTE: We haven't given you function signatures or docstrings for these, so
+# you'll need to write your own.
+
+#    (a) Write a function that takes a town name as a string and evaluates to
+#        `True` if it is your hometown, and `False` otherwise.
+def hometown(town):
+    """Given a town, evaluates if it is the correct town"""
+    if town == "Recife":
+        correct_town = True
+    else:
+        correct_town = False
+    return correct_town
+
+my_hometown = hometown("Recife")
+#    (b) Write a function that takes a first and last name as arguments and
+#        returns the concatenation of the two names in one string.
+def concatenate_names(first_name, last_name):
+    """Given a first and last name, returns full name concatenated"""
+    full_name = "%s" " " "%s" %(first_name, last_name)
+    return full_name
+
+given_name = concatenate_names("Letania", "Ferreira")
+
+#    (c) Write a function that takes a home town, a first name, and a last name
+#        as arguments, calls both functions from part (a) and (b) and prints
+#        "Hi, 'full name here', we're from the same place!", or "Hi 'full name
+#        here', I'd like to visit 'town name here'!" depending on what the function
+#        from part (a) evaluates to.
+def gretings_from_same_town(full_name, correct_town):
+    """given a home town and a full name, greets people and comment on town"""
+    if correct_town == True:
+        greetings = "Hi %s, we're from the same place!" %(full_name)
+    else:
+        greetings = "Hi %s, I'd like to visit %s!" %(full_name, correct_town)
+    return greetings
+
+gretings_from_same_town(given_name, my_hometown)
+
+
+
+###############################################################################
+
+# PART TWO
+
+#    (a) Write a function, `is_berry()`, which takes a fruit name as a string
+#        and returns a boolean if the fruit is a "strawberry", "raspberry", or
+#        "blackberry."
+
+#    (b) Write another function, shipping_cost(), which calculates shipping
+#        cost by taking a fruit name as a string and calling the `is_berry()`
+#        function within the `shipping_cost()` function. Your function should
+#        return 0 if is_berry() == True, and 5 if is_berry() == False.
+
+#    (c) Make a function that takes in a number and a list of numbers. It should
+#        return a new list containing the elements of the input list, along with
+#        given number, which should be at the end of the new list.
+
+#    (d) Write a function calculate_price to calculate an item's total cost by
+#        adding tax, and any fees required by state law.
+
+#        Your function will take as parameters (in this order): the base price of
+#        the item, a two-letter state abbreviation, and the tax percentage (as a
+#        two-digit decimal, so, for instance, 5% will be .05). If the user does not
+#        provide a tax rate it should default to 5%.
+
+#        CA law requires stores to collect a 3% recycling fee, PA requires a $2
+#        highway safety fee, and in MA, there is a Commonwealth Fund fee of $1 for
+#        items with a base price under $100 and $3 for items $100 or more. Fees are
+#        added *after* the tax is calculated.
+
+#        Your function should return the total cost of the item, including tax and
+#        fees.
+
+
+def is_berry(fruit):
+    """Determines if fruit is a berry
+
+    >>> is_berry("blackberry")
+    True
+
+    >>> is_berry("durian")
+    False
+
+    """
+    if fruit == "strawberry" or fruit == "blackberry" or fruit == "raspberry":
+        berry = True
+    else:
+        berry = False
+    return berry
+
+type_of_fruit = is_berry("mango")
+
+
+def shipping_cost(fruit):
+    """Calculates shipping cost of fruit
+
+    >>> shipping_cost("blackberry")
+    0
+
+    >>> shipping_cost("durian")
+    5
+
+    """
+    
+    if is_berry(fruit):
+        shipping = 0
+    else:
+        shipping = 5
+    return shipping
+
+shipping_cost("durian")
+
+
+def append_to_list(lst, num):
+    """Returns a new list consisting of the old list with the given number
+       added to the end.
+
+    >>> append_to_list([3, 5, 7], 2)
+    [3, 5, 7, 2]
+
+    """
+    new_list = []
+    for nums in lst:
+        new_list.append(nums)
+    new_list.append(num)
+    return new_list
+
+append_to_list([3, 5, 7], 2)
+
+def calculate_price(base_price, state='CA', taxes=.05):
+    """Calculate total price of an item, figuring in state taxes and fees.
+
+    >>> calculate_price(40, "CA")
+    43.26
+
+    >>> calculate_price(400, "NM")
+    420.0
+
+    >>> calculate_price(150, "OR", 0.0)
+    150.0
+
+    >>> calculate_price(60, "PA")
+    65.0
+
+    >>> calculate_price(38, "MA")
+    40.9
+
+    >>> calculate_price(126, "MA")
+    135.3
+
+    """
+    tax_amount = base_price * taxes
+    price_with_taxes = base_price + tax_amount
+   
+    california_fee = .03
+    
+
+    if state == "MA":
+        if base_price < 100:
+            fee_amount = 1
+        else:
+            fee_amount = 3
+    elif state == "PA":
+        fee_amount = 2
+    elif state == "CA":
+        fee_amount = price_with_taxes * california_fee
+    else:
+        fee_amount = 0
+
+    total_price = price_with_taxes + fee_amount
+    return total_price
+
+calculate_price(20)
+
+
+###############################################################################
+
+# PART THREE: ADVANCED
+
+# NOTE: We haven't given you function signatures and docstrings for these, so
+# you'll need to write your own.
+
+#    (a) Make a new function that takes in a list and any number of additional
+#        arguments, appends them to the list, and returns the entire list. Hint: this
+#        isn't something we've discussed yet in class; you might need to google how to
+#        write a Python function that takes in an arbitrary number of arguments.
+original_lst = [1, 56, 78, 4]
+new_lst = []
+
+def append_n_arguments_to_list(lst, *arg):
+    for item in original_lst:
+        new_lst.append(item)
+
+    new_lst.append(len(arg))
+
+    return new_lst
+append_n_arguments_to_list(original_lst, "house", "ball")
+
+#    (b) Make a new function with a nested inner function.
+#        The outer function will take in a word.
+#        The inner function will multiply that word by 3.
+#        Then, the outer function will call the inner function.
+#        Print the output as a tuple, with the original function argument
+#        at index 0 and the result of the inner function at index 1.
+
+#        Example:
+
+#        >>> outer("Balloonicorn")
+#        ('Balloonicorn', 'BalloonicornBalloonicornBalloonicorn')
+
+
+###############################################################################
+
+# END OF ASSESSMENT: You can ignore everything below.
+
+if __name__ == "__main__":
+    import doctest
+
+    print
+    result = doctest.testmod()
+    if not result.failed:
+        print "ALL TESTS PASSED. GOOD WORK!"
+    print
